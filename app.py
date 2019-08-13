006800230c1d88843e419203026fc5a2b60963db
zhangyan <15510684768@163.com>
2019-08-13 06:56:35 -0400

    使用wsgify装饰器

--- app.py
+++ app.py
@@ -1,24 +1,15 @@
-from wsgiref.simple_server import make_server
-import os
-from urllib.parse import parse_qs
 import webob
+from wsgiref.simple_server import make_server
+from webob.dec import wsgify
 
-def application(environ:dict,  start_response):
-    request = webob.Request(environ)
+@wsgify
+def application(request: webob.Request) -> webob.Response:
     name = request.params.get("name", 'anonymous')
     response = webob.Response()
     response.text = 'hello {}'.format(name)
     response.status_code = 200
     response.content_type = 'text/plain'
-    return response(environ, start_response)
-   # params = parse_qs(environ['QUERY_STRING'])
-   # name = params.get('name', ['anonymous'])[0]
-   # for k, v in environ.items():
-   #     if k not in os.environ.keys():
-   #         print('{} => {}'.format(k,v))
-    
-   # start_response('200 ok',[('Content-Type', 'text/plain')])
-   # return ["hello {}".format(name).encode()]
+    return response
 
 if __name__ == '__main__':
     server = make_server('0.0.0.0',8000, application)
