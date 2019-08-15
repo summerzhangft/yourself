4fcf2226a1f0025959113b2f11bc818776c43b8b
zhangyan <15510684768@163.com>
2019-08-14 22:28:42 -0400

    装饰器的路由注册

--- app.py
+++ app.py
@@ -4,22 +4,16 @@ import  webob
 from webob import Request, Response
 from webob import exc
 
-def hello(request: Request) -> Response:
-    name = request.params.get("name", 'anonymous')
-    response = Response()
-    response.text = 'hello {}'.format(name)
-    response.status_code = 200
-    response.content_type = 'text/plain'
-    return response
 
-def index(request: Request) -> Response:
-    return Response(body = 'hello world',content_type='text/plain')
 
 class Application:
     ROUTER = {}
     @classmethod
-    def register(cls,path, handler):
-        cls.ROUTER[path] = handler
+    def register(cls,path):
+        def wrap(handler):
+            cls.ROUTER[path] = handler
+            return handler
+        return wrap
 
     @wsgify
     def __call__(self, request: Request) -> Response:
@@ -28,9 +22,25 @@ class Application:
         except KeyError:
             return exc.HTTPNotFound("not found")
 
+
+
+@Application.register("/hello")
+def hello(request: Request) -> Response:
+    name = request.params.get("name", 'anonymous')
+    response = Response()
+    response.text = 'hello {}'.format(name)
+    response.status_code = 200
+    response.content_type = 'text/plain'
+    return response
+
+
+@Application.register("/")
+def index(request: Request) -> Response:
+    return Response(body = 'hello world',content_type='text/plain')
+
+
+
 if __name__ == '__main__':
-    Application.register('/hello',hello)
-    Application.register('/',index)
 
     server = make_server('0.0.0.0', 8000, Application())
     try:
